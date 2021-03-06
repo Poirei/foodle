7eee7206f1f506aafc8e78934d69e3da872ee483
K.Poirei Ngamba Singha <poirei4@gmail.com>
2021-03-06 22:51:30 +0530

    minor changes.

--- src/pages/SignInSignUpPage/SignInSignUpPage.js
+++ src/pages/SignInSignUpPage/SignInSignUpPage.js
@@ -1,8 +1,5 @@
-import { Grid, Paper, TextField } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
 import React from "react";
-import SignIn from "../../components/forms/sign-in/SignIn";
-import SignUp from "../../components/forms/sign-up/SignUp";
+import { SignIn, SignUp } from "../../components/forms";
 import "./SignInSignUpPage.scss";
 
 const SignInSignUpPage = () => {
